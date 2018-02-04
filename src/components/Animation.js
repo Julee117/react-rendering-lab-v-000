e43b43504281c65ea874ccb8540bd16c3ce42cac
github username <juliannne@gmail.com>
2018-02-04 18:07:31 -0500

    Done.

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -25,6 +25,10 @@ class Animation extends React.Component {
       );
   };
 
+  componentWillUpdate(nextProps, nextState) {
+    return this.showLoadingBar();
+  }
+
   showLoadingBar = () => {
     const progressBar = document.getElementById("progress-bar");
     progressBar.className = "off on";
